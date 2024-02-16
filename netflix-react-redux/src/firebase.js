4b183e3e30ed3dee5dc5ad027cd368c2d8e58f9d
marisiromanillos <marisiromanillos@gmail.com>
2024-02-16 12:25:12 +0000

    first commit

---
+++ netflix-react-redux/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvXmdyJKBenIeVFQJ6JUukkk0TCbInDCk",
+  authDomain: "netflix-react-redux-c82bc.firebaseapp.com",
+  projectId: "netflix-react-redux-c82bc",
+  storageBucket: "netflix-react-redux-c82bc.appspot.com",
+  messagingSenderId: "268648993767",
+  appId: "1:268648993767:web:713ae8a593811a1f89e342",
+};
